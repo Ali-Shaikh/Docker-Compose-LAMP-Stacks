6d46f57753afe34855392ebba69343ef583010f6
Ali Shaikh <aliakbarshaikh@live.co.uk>
2018-05-16 00:13:40 +0500

    Load a Database Dump on Container Create. MySQL Config Improvements

---
+++ Apache2-MySQL57-PHP53-SphinxSearch/www/index.php
@@ -0,0 +1,3 @@
+<?php
+echo "<h2 style='text-align:center;'>This is a Docker LAMP Stack by Ali Shaikh - ali.az.shaikh@gmail.com!<h2>";
+phpinfo();
--- Apache2-MySQL57-PHP56-phpMyAdmin-Adminer/www/db_test.php
+++ Apache2-MySQL57-PHP56-phpMyAdmin-Adminer/www/db_test.php
@@ -1,5 +1,5 @@
 <?php
-$link = mysqli_connect("mysql", "root", "root", "test-db");
+$link = mysqli_connect("mysql", "root", "root", "my_db");
 if (!$link) {
 	echo "Error: Unable to connect to MySQL." . PHP_EOL;
 	echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
